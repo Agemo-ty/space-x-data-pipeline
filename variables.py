d0b48a4906a89f46900c249226c0af34f1d295f7
“Agemo <agemotemitayonaomi@gmail.com>
2025-01-07 16:50:03 +0000

    Python basics assignment

---
+++ variables.py
@@ -0,0 +1,10 @@
+#Integer
+number_of_launches = 50
+#Integer
+number_of_rockets = 60
+#Float
+launch_success_rate = 0.89
+
+print(number_of_launches)
+print(number_of_rockets)
+print(launch_success_rate)
